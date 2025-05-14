149612771b82743d1f8623d843c4c1aa23da85e5
Willie-Conway <hire.willie.conway@gmail.com>
2025-05-13 21:28:01 -0400

    Fix syntax error in test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -27,7 +27,7 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 class TestAccountService(TestCase):
     """Account Service Tests"""
-;71R51R;71R;51R
+
     @classmethod
     def setUpClass(cls):
         """Run once before all tests"""
@@ -252,5 +252,4 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check for the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
-        
-
+    
